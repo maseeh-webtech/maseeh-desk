48c37730784ee06e5dcc003870f562b99935e3d5
Kye Burchard <kyeb@mit.edu>
2020-08-31 02:28:58 -0400

    Convert notfound to typescript

---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+const NotFound = () => (
+  <div>
+    <h1>404 Not Found</h1>
+    <p>The page you requested couldn't be found.</p>
+  </div>
+);
+
+export default NotFound;
